a3f14b740bc7f81e6ad1f70d27fd04468c59ad86
henriqueolivgp <henrique.o@ipvc.pt>
2024-02-01 11:24:22 +0000

    chnages

---
+++ portfolio/src/client/components/BgAnimation.tsx
@@ -0,0 +1,24 @@
+import '../theme/bganimation.css';
+
+
+export const BgAnimation = () => {
+
+  return (
+    <>
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+};
--- portfolio/src/client/screens/AboutMe.tsx
+++ portfolio/src/client/screens/AboutMe.tsx
@@ -1,13 +1,15 @@
+import { BgAnimation } from "../components/BgAnimation";
+
+
 export const AboutMe = () => {
   return (
     <>
+      <BgAnimation />
       <div className="bg-gradient-to-r from-leftcolor to-rigthcolor h-screen">
         <div className="container mx-auto  ">
           <div className="content mx-auto ">
             <h1 className=" text-white text-center">About me</h1>
-            <div className=" p-20">
-
-            </div>
+            <div className=" p-20"></div>
           </div>
         </div>
       </div>

0012edb59fe61e4db2ed4aa9e3f3a131b3260261
henriqueolivgp <henrique.o@ipvc.pt>
2024-07-27 20:17:21 +0100

    install typewriter-effect and create typewriter and add in introduction component

---
+++ portfolio/src/client/components/tipeWriter.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { HtmlHTMLAttributes } from "react";
+import Typewriter from "typewriter-effect";
+
+interface TypewriterTextProps {
+  text: string; // Modificado para uma única string
+  divProps?: HtmlHTMLAttributes<HTMLDivElement>;
+}
+
+export function TypewriterText({ text, divProps }: TypewriterTextProps) {
+  return (
+    <div {...divProps}>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString(text) // Use a string diretamente
+            .pauseFor(10000)  // Pausa antes de apagar
+            .deleteAll()      // Apaga o texto
+            .start();         // Inicia o efeito de digitação
+        }}
+        options={{
+          autoStart: true,
+          loop: true,        // Continua a repetir
+        }}
+      />
+    </div>
+  );
+}

26a4ca40dddd8b2d036e9cd211846ef817291dfe
henriqueolivgp <henrique.o@ipvc.pt>
2025-01-27 18:38:29 +0000

    delete screens contact me and technologies

--- portfolio/src/client/screens/contactme.tsx
+++
@@ -1,7 +0,0 @@
-export const ContactMe = () => {
-  return (
-    <>
-      <h1 id="Contact-me" className=" text-left text-white text-4xl ">Contact-me</h1>
-    </>
-  );
-};
--- portfolio/src/client/screens/technologies.tsx
+++
@@ -1,7 +0,0 @@
-export const Technologies = () => {
-  return (
-    <>
-      <h1 id="technologies" className=" text-left text-white text-4xl ">Technologies</h1>
-    </>
-  );
-};

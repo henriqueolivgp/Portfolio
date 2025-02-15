574f69add3ba6b6c8a88dc202d93f2d5956c27f6
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-15 12:12:02 +0000

    change project images location into public folder and add expertiNotes image innto images function

--- portfolio/src/client/utils/images.ts
+++ portfolio/src/client/utils/images.ts
@@ -1,10 +1,11 @@
 // Importa todas as imagens
 import CRImage from "../assets/projects/CR.png";
+import ExpertNotes from "../assets/projects/Expert-Notes.png";
 
 // Exporta as imagens em um objeto
 const images = {
   CRImage,
-
+  ExpertNotes,
 };
 
 export default images;

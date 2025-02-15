bccec3077f9310ae705b173da961ab2f51e459d3
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-15 12:13:36 +0000

    create a export type for all app

---
+++ portfolio/src/client/DTO/projects.ts
@@ -0,0 +1,12 @@
+type Project = {
+  id: number;
+  name: string;
+  description: string;
+  technologies: string[];
+  image: string;
+  repository: string;
+  live: string;
+  features: string[];
+};
+
+export type { Project };

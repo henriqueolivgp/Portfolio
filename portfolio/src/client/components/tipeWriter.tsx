692306e59aaa413c3a98664553b1479515a66a3e
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-18 00:51:31 +0000

    add a blue color in text

--- portfolio/src/client/components/tipeWriter.tsx
+++ portfolio/src/client/components/tipeWriter.tsx
@@ -1,8 +1,8 @@
-"use client";
-import { HtmlHTMLAttributes } from "react";
+
+import { ComponentProps, HtmlHTMLAttributes } from "react";
 import Typewriter from "typewriter-effect";
 
-interface TypewriterTextProps {
+interface TypewriterTextProps extends ComponentProps<'div'> {
   text: string; // Modificado para uma única string
   divProps?: HtmlHTMLAttributes<HTMLDivElement>;
 }
@@ -11,9 +11,10 @@ export function TypewriterText({ text, divProps }: TypewriterTextProps) {
   return (
     <div {...divProps}>
       <Typewriter
+
         onInit={(typewriter) => {
           typewriter
-            .typeString(text) // Use a string diretamente
+            .typeString(`<span class="text-blue-500">${text}</span>`) // Use a string diretamente
             .pauseFor(5000)  // Pausa antes de apagar
             .deleteAll()      // Apaga o texto
             .start();         // Inicia o efeito de digitação

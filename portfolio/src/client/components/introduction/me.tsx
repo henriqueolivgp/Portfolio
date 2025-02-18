8cf3b76ecaae653a3a43778c5e247f1211a59173
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-18 00:51:45 +0000

    Create a me component for intro

---
+++ portfolio/src/client/components/introduction/me.tsx
@@ -0,0 +1,17 @@
+export const Me = () => {
+  return (
+    <>
+      <div className="flex justify-center md:justify-end">
+        {/* Imagem para telas médias (md) ou maiores */}
+        <a href="https://github.com/henriqueolivgp" target="_blank">
+          <div className="relative">
+            {/* Gradiente no fundo */}
+            <div className="bg-gradient-to-b from-blue-900 via-blue-900 to-slate-950 h-80 absolute bottom-0 w-full rounded-t-lg"></div>
+            {/* Imagem acima do gradiente */}
+            <img src="me.png" alt="me" className="w-[514px] h-auto relative z-10" />
+          </div>
+        </a>
+      </div>
+    </>
+  );
+};

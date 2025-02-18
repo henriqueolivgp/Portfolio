92a2dc7c73f7b3012a079934d1d035758d8bf23d
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-18 13:09:02 +0000

    change the footer import to components/footer

--- portfolio/src/client/routes/App.tsx
+++ portfolio/src/client/routes/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/navbar";
-import { Footer } from "../screens/footer";
+import { Footer } from "../components/footer";
 
 function App() {
   return (

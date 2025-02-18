9c5f1f0098b22bd062c2d03fc0196d15e7af137b
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-02-18 12:30:33 +0000

    change all text in about me

--- portfolio/src/client/screens/AboutMe.tsx
+++ portfolio/src/client/screens/AboutMe.tsx
@@ -1,3 +1,4 @@
+import { MeAbout } from "../components/AboutMe/me-about";
 import { Skills } from "../components/AboutMe/skills";
 
 export const AboutMe = () => {
@@ -5,55 +6,65 @@ export const AboutMe = () => {
     <>
       <div
         id="about-me"
-        className=" xs:pt-4 bg-gray-900 flex justify-center min-h-screen"
+        className="flex p-10 items-center xs:pt-4 bg-gray-900 justify-center min-h-screen"
       >
-        <div className="pl-20 pt-28 flex-none">
-          <h1 className="relative font-bold mt-4 text-left text-gray-300 text-3xl ">
-            About-me
-          </h1>
-          <svg
-            width="146"
-            height="12"
-            viewBox="0 0 136 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M3.14131 8.21839C103.179 -0.581582 131.229 2.82004 132.748 5.62084"
-              stroke="#EFF6FF"
-              stroke-width="4.56445"
-              stroke-linecap="round"
-            />
-          </svg>
-        </div>
-
-        <div className="flex flex-col md:flex-row md:items-center md:justify-center md:mt-10 xs:mt-6 w-full">
-          {/* Imagem à esquerda */}
-          <div className="md:w-1/3 flex justify-center">
-            <img
-              className="md:w-[400px] xs:w-72 rounded-md"
-              src="/me.jpg"
-              alt="My portrait"
-            />
-          </div>
-          {/* Texto à direita */}
-          <div className="md:w-2/3 mt-8 md:mt-0 flex flex-col">
-            <p className="text-gray-400 md:text-lg xs:text-sm xs:text-center md:text-left font-bold max-w-4xl leading-relaxed">
-              I'm a Full-stack developer graduated in the course of Technology
-              and Programming of Information Systems, TPSI at the Polytechnic
-              Institute of Viana do Castelo-IPVC. I work with state-of-the-art
-              technologies: React, React Native, Next.js, PostgreSQL,
-              TypeScript, and JavaScript, besides developing REST and RESTful
-              APIs. I love to program in my free time. <br></br>See the final
-              result of my projects, making sure everything is working
-              perfectly.
-            </p>
-            <div className="flex-wrap mt-10 w-auto">
-              <h1 className="text-xl text-gray-300 mb-8 font-semibold">
-                My Skills
+        <div className="flex justify-center items-center w-full h-auto gap-8 ">
+          <MeAbout />
+          <div className="flex flex-col w-[40%]">
+            <div className="mb-6">
+              <h1 className="flex text-left font-bold text-gray-300 text-3xl ">
+                About Me
               </h1>
-              <Skills />
+              <svg
+                width="146"
+                height="12"
+                viewBox="0 0 136 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.14131 8.21839C103.179 -0.581582 131.229 2.82004 132.748 5.62084"
+                  stroke="#EFF6FF"
+                  stroke-width="4.56445"
+                  stroke-linecap="round"
+                />
+              </svg>
             </div>
+            <p className=" font-medium text-gray-300 text-lg text-left">
+              I have been studying{" "}
+              <span className="text-sky-600">
+                Systems Development for four years
+              </span>
+              , gaining experience in{" "}
+              <span className="text-sky-600">
+                web, mobile, and backend development
+              </span>
+              . I completed a professional{" "}
+              <span className="text-sky-600">
+                university course in Technology and Programming of Information
+                Systems
+              </span>{" "}
+              at the Polytechnic Institute of Viana do Castelo (IPVC) from 2021
+              to 2024.
+              <br />
+              <br />
+              My main technologies include{" "}
+              <span className="text-sky-600">
+                HTML, CSS, JavaScript, React, and Next.js
+              </span>{" "}
+              for web development,{" "}
+              <span className="text-sky-600">React Native</span> for mobile, and
+              on the backend, I use
+              <span className="text-sky-600">
+                Node.js with Fastify or Nest.js
+              </span>
+              , integrating <span className="text-sky-600">PostgreSQL</span> as
+              a database and communicating via{" "}
+              <span className="text-sky-600">REST API</span>.<br />
+              <br />
+              Get in touch so we can build something great together.
+            </p>
+            {/* <Skills /> */}
           </div>
         </div>
       </div>

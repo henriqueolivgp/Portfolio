791237ba8eb747df943446da317c7a2d0e8f237a
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-05-14 11:16:08 +0100

    remove paddings

--- portfolio/src/client/screens/projects.tsx
+++ portfolio/src/client/screens/projects.tsx
@@ -3,8 +3,8 @@ import { ProjectsGroup } from "../components/Projects/projectGroup";
 export const Projects = () => {
   return (
     <>
-      <div id="projects" className="relative pt-28 ">
-        <div className="pl-20">
+      <div id="projects" className="relative">
+   
           <h1 className="relative font-bold mt-4 text-left text-gray-300 text-3xl ">
             Projects
           </h1>
@@ -26,7 +26,7 @@ export const Projects = () => {
         <div className="relative flex mt-16 w-full justify-center items-center">
           <ProjectsGroup />
         </div>
-      </div>
+ 
     </>
   );
 };

0c61c00000231de7f64d5d4491254012e1016166
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-05-14 14:54:44 +0100

    Add a coming soon in dropdown menu

--- portfolio/src/client/components/navbarComponents/dropdown.tsx
+++ portfolio/src/client/components/navbarComponents/dropdown.tsx
@@ -66,7 +66,7 @@ export const DropDown = () => {
                 href="#mobile-projects"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
-                Mobile Projects
+                Mobile Projects (Coming Soon)
               </a>
             </li>
           </ul>

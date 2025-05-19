c492c6634985ddda0d2d0893b0f3540b776f89f5
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-05-19 23:18:28 +0100

    delete 3 unused files

--- portfolio/src/client/components/sparator.tsx
+++
@@ -1,9 +0,0 @@
-export const Separator = () => {
-  return (
-    <>
-      <div className="mx-">
-        <div className="w-full h-72 bg-gradient-to-r from-BackgroundL to-BackgroundR"></div>
-      </div>
-    </>
-  );
-};

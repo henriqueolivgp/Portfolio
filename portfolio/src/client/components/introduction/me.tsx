0ce9447596da809adf39b2dac2bcc77124080556
henriqueolivgp <henrique.o@ipvc.pt>
2025-05-24 23:46:29 +0100

    Introduction responsivity working

--- portfolio/src/client/components/introduction/me.tsx
+++ portfolio/src/client/components/introduction/me.tsx
@@ -4,11 +4,11 @@ export const Me = () => {
       <div className="flex justify-center md:justify-end">
         {/* Imagem para telas médias (md) ou maiores */}
         <a href="https://github.com/henriqueolivgp" target="_blank">
-          <div className="relative">
+          <div className="relative w-full">
             {/* Gradiente no fundo */}
-            <div className="bg-gradient-to-b from-blue-900 via-blue-900 to-slate-950 h-80 absolute bottom-0 w-full rounded-t-lg"></div>
+            <div className="bg-gradient-to-b from-blue-900 via-blue-900 to-slate-950 h-80 xxs:h-64 md:h-52 absolute bottom-0 w-full rounded-t-lg"></div>
             {/* Imagem acima do gradiente */}
-            <img src="me.png" alt="me" className="w-[514px] h-auto relative z-10" />
+            <img src="me.png" alt="me" className="w-[514px] xxs:w-96 h-auto relative z-10" />
           </div>
         </a>
       </div>
--- portfolio/src/client/screens/intoduction.tsx
+++ portfolio/src/client/screens/intoduction.tsx
@@ -25,82 +25,82 @@ export const Introduction = (props: IntroductionProps) => {
         <div className="absolute top-0 left-0 h-screen w-full bg-gradient-to-t from-black/100 to-black/35  "></div>
         <div className="absolute bottom-0 left-0 right-0 h-1/4 bg-gradient-to-t from-black/100 to-black/0 pointer-events-none"></div>
 
-        <div className="container mx-auto space-y-2 ">
-          <div className="content flex flex-col justify-center h-screen parallax">
-            <div className="flex md:justify-between xs:justify-center xs:items-center xs:flex">
-              <div className="bg-transparent font-normal">
-                <div>
-                  <p className="md:mb-2 md:tracking-tight text-gray-300  text-4xl lg:text-6xl xs:text-2xl ">
-                    Hi,
-                  </p>
-                  <p className="md:mb-2 tracking-tight text-gray-300  text-4xl xl:text-6xl xs:text-2xl ">
-                    I’m Henrique Oliveira,
-                  </p>
-                  <span className="tracking-tight text-gray-300  text-4xl xl:text-6xl xs:text-2xl ">
-                    <TypewriterText text="Full-Stack Developer." />
-                  </span>
-                  <svg
-                    width="100%"
-                    height="32"
-                    viewBox="0 0 713 32"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8 24.6382C314.835 -2.35291 400.867 8.08049 405.529 16.6711"
-                      stroke="#60A5FA"
-                      stroke-width="14"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                </div>
-                <div className="flex gap-4 ml-[6%]">
-                  <a
-                    href="https://www.linkedin.com/in/henrique-oliveira-gp"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button
-                      type="button"
-                      className=" flex items-center gap-x-2 text-gray-200 border-gray-400 border-2 md:text-lg xs:text-sm bg-slate-950 hover:bg-slate-800 font-medium rounded-lg md:px-5 xs:p-2 py-2.5 mt-4"
-                    >
-                      <Linkedin />
-
-                      <ShinyText
-                        text="LinkedIn"
-                        disabled={false}
-                        speed={3}
-                        className="custom-class"
+        <div className="container mx-auto space-y-2 parallax h-screen flex justify-center items-center w-full">
+            <div className="flex w-full lg:p-10 lgg:p-10 xxs:flex-wrap gap-4 lg:justify-between xxs:justify-center items-center">
+              <div className="flex w-full xxs:w-full sm:w-1/2 md:w-1/2 bg-transparent font-normal">
+                <div className="flex flex-col">
+                  <section className="flex flex-col">
+                    <p className="md:mb-2 md:tracking-tight text-gray-300  text-4xl xl:text-4xl xs:text-2xl lg:text-3xl ">
+                      Hi,
+                    </p>
+                    <p className=" md:mb-2 tracking-tight text-gray-300  text-4xl xl:text-4xl xs:text-2xl lg:text-3xl ">
+                      I’m Henrique Oliveira,
+                    </p>
+                    <span className="tracking-tight text-gray-300  text-4xl xl:text-4xl xs:text-2xl lg:text-3xl ">
+                      <TypewriterText text="Full-Stack Developer." />
+                    </span>
+                  </section>
+                  <div className="flex ">
+                    <svg className="w-4/5 h-8" viewBox="0 0 713 32">
+                      <path
+                        className=""
+                        d="M8 24.6382C314.835 -2.35291 400.867 8.08049 405.529 16.6711"
+                        stroke="#60A5FA"
+                        stroke-width="14"
+                        stroke-linecap="round"
                       />
-                    </button>
-                  </a>
-                  <a
-                    href="https://github.com/henriqueolivgp"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button
-                      type="button"
-                      className="flex items-center gap-x-2 text-gray-200 border-gray-400 border-2 md:text-lg xs:text-sm bg-slate-950 hover:bg-slate-800 font-medium rounded-lg md:px-5 xs:p-2 py-2.5 mt-4 "
+                    </svg>
+                  </div>
+                  <div className="flex gap-4 ml-[6%]">
+                    <a
+                      href="https://www.linkedin.com/in/henrique-oliveira-gp"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <Github size={24} />
+                      <button
+                        type="button"
+                        className=" flex items-center gap-x-2 text-gray-200 border-gray-400 border-2 md:text-lg xs:text-sm bg-slate-950 hover:bg-slate-800 font-medium rounded-lg md:px-5 xs:p-2 py-2.5 mt-4"
+                      >
+                        <Linkedin />
 
-                      <ShinyText
-                        text="Github"
-                        disabled={false}
-                        speed={3}
-                        className="custom-class"
-                      />
-                    </button>
-                  </a>
+                        <ShinyText
+                          text="LinkedIn"
+                          disabled={false}
+                          speed={3}
+                          className="custom-class"
+                        />
+                      </button>
+                    </a>
+                    <a
+                      href="https://github.com/henriqueolivgp"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button
+                        type="button"
+                        className="flex items-center gap-x-2 text-gray-200 border-gray-400 border-2 md:text-lg xs:text-sm bg-slate-950 hover:bg-slate-800 font-medium rounded-lg md:px-5 xs:p-2 py-2.5 mt-4 "
+                      >
+                        <Github size={24} />
+
+                        <ShinyText
+                          text="Github"
+                          disabled={false}
+                          speed={3}
+                          className="custom-class"
+                        />
+                      </button>
+                    </a>
+                  </div>
                 </div>
               </div>
               {}
-              <Me />
+              <section className="flex lg:w-1/3 sm:w-1/2 xxs:w-full">
+                <Me />
+              </section>
             </div>
           </div>
         </div>
-      </div>
+      
     </>
   );
 };

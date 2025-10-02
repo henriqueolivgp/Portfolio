88eed0f8bcf5ae6e0ddbca09814f04d9e530f450
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-10-02 15:41:22 +0100

    style: change my photo size in introduction

--- portfolio/src/client/components/introduction/me.tsx
+++ portfolio/src/client/components/introduction/me.tsx
@@ -6,9 +6,9 @@ export const Me = () => {
         <a href="https://github.com/henriqueolivgp" target="_blank">
           <div className="relative mx-auto w-full">
             {/* Gradiente no fundo */}
-            <div className="bg-gradient-to-b from-blue-900 via-blue-900 to-slate-950 h-80 xs:h-44 md:h-52 absolute bottom-0 w-60 rounded-t-lg"></div>
+            <div className="bg-gradient-to-b from-blue-900 via-blue-900 to-slate-950 xl:h-80 w-full xs:h-44 md:h-52 absolute bottom-0 rounded-t-lg"></div>
             {/* Imagem acima do gradiente */}
-            <img src="me.png" alt="me" className="w-[514px] xs:w-60 h-auto relative z-10" />
+            <img src="me.png" alt="me" className="sm:w-[514px] xs:w-60 sm:h-auto relative z-10" />
           </div>
         </a>
       </div>
